94291ebb0be943603221156adc8d08747a7eaa2d
Indransh <indranshpratap@gmail.com>
2026-07-06 11:06:47 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>arr(26,0);
+if(s.size()!=t.size()) return false;
+       for(char ch : s){
+        arr[ch-'a']++;
+
+       }
+
+       for(char ch : t){
+        arr[ch-'a']--;
+       }
+
+       for(int i=0; i<arr.size(); i++){
+        if(arr[i]>0) return false;
+       }
+
+       return true;
+    }
+};
